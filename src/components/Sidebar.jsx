069672fbdc02c4e7562452cc40e9d358d4fbaace
Jasmineodaniel <jasminedaniel046@gmail.com>
2026-04-23 09:21:40 -0700

    chore(assets): replace placeholder vectors with production image assets

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import { useTheme } from '../context/ThemeContext'
+﻿import { useTheme } from '../context/ThemeContext'
 import '../styles/sidebar.css'
 
 function Sidebar() {
@@ -38,7 +38,7 @@ function Sidebar() {
         <div className="sidebar__divider" />
 
         <div className="sidebar__avatar">
-          <img src="jjgirl.jpg" alt="User avatar" />
+<img src="ivan.png" alt="User avatar" />
         </div>
       </div>
     </aside>
@@ -46,3 +46,4 @@ function Sidebar() {
 }
 
 export default Sidebar
+
